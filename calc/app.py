04abcabe4b6884f8e4446c7ae244a2a852edc3c3
Luke <lukeelder@me.com>
2020-10-25 20:41:15 -0400

    Completed functionality

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,40 @@
-# Put your app in here.
+from flask import Flask , request
+from operations import *
+app = Flask(__name__)
+
+@app.route("/add")
+def math_add():
+    """Add a and b."""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(add(a, b))
+
+@app.route("/sub")
+def math_sub():
+    """Subtract a and b."""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(sub(a, b))
+
+@app.route("/mult")
+def math_mult():
+    """Multiply a and b."""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(mult(a, b))
+
+@app.route("/div")
+def math_div():
+    """Divide a and b."""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(div(a, b))
+
+functions = {'add': add, 'sub': sub, 'mult': mult, 'div': div}
+
+@app.route("/math/<operator>")
+def all_math(operator):
+    """Perform given operation on a and b."""
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    return str(functions[operator](a,b))
